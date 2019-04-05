ac9589d11aeb6f47f0937bd4aaa12a2bd31bc173
inigodm <inigo.delgado@gmail.com>
2019-04-06 00:01:46 +0200

    Egin behar ziren metudu guztiak egindak

--- src/main/kotlin/org/inigo/izenak/IzenFinder.kt
+++ src/main/kotlin/org/inigo/izenak/IzenFinder.kt
@@ -5,12 +5,20 @@ import org.jsoup.nodes.Document
 
 class WebScrapper{
     fun getHtmlDocument(url: String): Document = throwsServiceException{
-        return@throwsServiceException Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get()
+        return@throwsServiceException Jsoup.connect("$url&lang=es").userAgent("Mozilla/5.0").timeout(100000).get()
     }
 
     fun obtainIzenDatakFrom(doc: Document): List<IzenData> {
-        return doc.select("a.list-group-item").map { IzenData(izena=it.text(), esanNahiURL=it.attr("href"))}
+        return doc.select("a.list-group-item").map { IzenData(izena=it.text(), esanahia=obtainIzenEsanahia(getHtmlDocument(it.attr("href"))))}
+    }
+
+    fun obtainIzenEsanahia(doc: Document): String {
+        return doc.select("div.eoda-azalpena").first().text()
+    }
+
+    fun obtainNextPageUrl(doc: Document): String {
+        return doc.select("ul.pagination").get(1).getElementsByTag("a").attr("href")
     }
 }
 
-data class IzenData(var izena: String, var esanNahia: String = "", var esanNahiURL: String){}
+data class IzenData(var izena: String, var esanahia: String = "", var esanNahiURL: String=""){}
--- src/test/kotlin/org/inigo/izenak/html_is_parsed.kt
+++ src/test/kotlin/org/inigo/izenak/html_is_parsed.kt
@@ -2,21 +2,40 @@ package org.inigo.izenak
 
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class HTML_scrappes{
-    val url: String = "https://www.euskaltzaindia.eus/index.php?option=com_eoda&Itemid=469&lang=eu&sexua=E&view=izenak"
+    val url: String = "https://www.euskaltzaindia.eus/index.php?option=com_eoda&Itemid=469&lang=es&sexua=E&view=izenak"
     @Test
-    fun if_gets_all_20_names_from_a_page(){
+    fun if_gets_all_20_izenData_from_a_page(){
         val ws : WebScrapper = WebScrapper()
         var names: List<IzenData> = ws.obtainIzenDatakFrom(ws.getHtmlDocument(url))
         assertEquals(20, names.size)
+        names.forEach{ println("${it.izena} --> ${it.esanahia}") }
     }
 
-    fun if_get_all_name_links_from_a_page(){
-
-    }
-
+    @Test
     fun if_get_description_from_a_name(){
+        val ws : WebScrapper = WebScrapper()
+        var url: String = "https://www.euskaltzaindia.eus/es/?option=com_eoda&Itemid=792&view=izenak&testua=Abeli%C3%B1e"
+        var izenInfo: String = ws.obtainIzenEsanahia(ws.getHtmlDocument(url))
+        assertEquals("Nombre propuesto en el Santoral publicado por Sabino Arana y Koldo Elizalde como equivalente de Avelina " +
+                "/ Aveline. Procede de la ciudad italiana de Avelino y se difundió gracias a San Andrés Avelino, nacido en " +
+                "ella. Su festividad se celebra el 10 de noviembre.", izenInfo)
+    }
 
+    @Test
+    fun if_get_link_to_next_page(){
+        val ws : WebScrapper = WebScrapper()
+        var next: String = ws.obtainNextPageUrl(ws.getHtmlDocument(url))
+        assertTrue { next.startsWith("https://") }
+    }
+
+    @Test
+    fun if_last_page_has_no_next_url(){
+        var url = "https://www.euskaltzaindia.eus/index.php?option=com_eoda&Itemid=469&view=izenak&sexua=E&nonhasi=Zutoia"
+        val ws : WebScrapper = WebScrapper()
+        var next: String = ws.obtainNextPageUrl(ws.getHtmlDocument(url))
+        assertTrue { next.startsWith("") }
     }
 }
