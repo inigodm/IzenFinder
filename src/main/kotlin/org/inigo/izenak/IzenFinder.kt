24bd0bc6e1a9a53ae17cbc3b33b0bb69be9526d7
inigodm <inigo.delgado@gmail.com>
2019-04-06 00:42:28 +0200

    Webgune izen gustiak eta beraien esanahiak har daiteke. Hemendik aurrera ahal baldin badut Corroutinak erabiliko ditut denborak murrizteko

--- src/main/kotlin/org/inigo/izenak/IzenFinder.kt
+++ src/main/kotlin/org/inigo/izenak/IzenFinder.kt
@@ -4,12 +4,16 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
 class WebScrapper{
+    var URL = "https://www.euskaltzaindia.eus/index.php?option=com_eoda&Itemid=469&lang=eu&sexua=E&view=izenak"
+
     fun getHtmlDocument(url: String): Document = throwsServiceException{
         return@throwsServiceException Jsoup.connect("$url&lang=es").userAgent("Mozilla/5.0").timeout(100000).get()
     }
 
     fun obtainIzenDatakFrom(doc: Document): List<IzenData> {
-        return doc.select("a.list-group-item").map { IzenData(izena=it.text(), esanahia=obtainIzenEsanahia(getHtmlDocument(it.attr("href"))))}
+        return doc.select("a.list-group-item").map {
+                    println("Data for ${it.text()}");
+              IzenData(izena=it.text(), esanahia=obtainIzenEsanahia(getHtmlDocument(it.attr("href"))))}
     }
 
     fun obtainIzenEsanahia(doc: Document): String {
@@ -19,6 +23,19 @@ class WebScrapper{
     fun obtainNextPageUrl(doc: Document): String {
         return doc.select("ul.pagination").get(1).getElementsByTag("a").attr("href")
     }
+
+    fun obtainAllNames(): List<IzenData> {
+        var url = URL;
+        var res = mutableListOf<IzenData>()
+        var webDoc: Document
+        while(url != "") {
+            webDoc = getHtmlDocument(url)
+            res.addAll(obtainIzenDatakFrom(webDoc))
+            url = obtainNextPageUrl(webDoc)
+            Thread.sleep(1000)
+        }
+        return res
+    }
 }
 
-data class IzenData(var izena: String, var esanahia: String = "", var esanNahiURL: String=""){}
+data class IzenData(var izena: String, var esanahia: String = "")
--- src/test/kotlin/org/inigo/izenak/izenak_are_obtained.kt
+++ src/test/kotlin/org/inigo/izenak/izenak_are_obtained.kt
@@ -1,2 +1,14 @@
 package org.inigo.izenak
 
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class process_is_done{
+    @Test
+    fun if_names_are_getted(){
+        val ws : WebScrapper = WebScrapper()
+        val names: List<IzenData> = ws.obtainAllNames()
+        names.forEach{ println("'${it.izena}','${it.esanahia}'") }
+        assertTrue { names.size > 0 }
+    }
+}
